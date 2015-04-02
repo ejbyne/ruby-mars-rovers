8b556c453a64f3a44abbbcfeabd49e3046034620
Edward Byne <ejbyne@gmail.com>
2015-04-02 08:38:35 +0200

    Amends controller so feature test for invalid coords now passing

--- app/controllers/plateau.rb
+++ app/controllers/plateau.rb
@@ -1,10 +1,15 @@
 class MarsRovers < Sinatra::Base
 
   post '/plateau' do
-    plateau = Plateau.new({ coords: params[:coords], cell_class: Cell })
-    session[:mission_control] = MissionControl.new({ plateau: plateau, rover_class: Rover })
-    flash[:notice] = 'Coordinates generated'
-    redirect '/rover/select'
+    begin
+      plateau = Plateau.new({ coords: params[:coords], cell_class: Cell })
+      session[:mission_control] = MissionControl.new({ plateau: plateau, rover_class: Rover })
+      flash[:notice] = 'Coordinates generated'
+      redirect '/rover/select'
+    rescue => error_message
+      flash[:error] = "#{error_message}"
+      redirect '/'
+    end
   end
 
 end
